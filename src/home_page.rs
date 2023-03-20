6a704d7740d1047e0dfe1b3d69dc332b7442a4e0
Eric Xiao <ericxiao0000@gmail.com>
2023-03-19 19:32:15 -0700

    saving past test results in localstorage

--- src/home_page.rs
+++ src/home_page.rs
@@ -15,14 +15,51 @@ pub fn get_next_start_time() -> i64 {
     get_now() + rand::thread_rng().gen_range(1500, 5000)
 }
 
+pub fn get_stored_results() -> Vec<TestResult> {
+    let storage = leptos::window().local_storage();
+
+    if let Ok(storage) = storage {
+        if let Some(storage) = storage {
+            let results_str = storage.get("results");
+            if let Ok(results_str) = results_str {
+                if let Some(results_str) = results_str {
+                    return results_str
+                        .lines()
+                        .map(str::parse)
+                        .filter(Result::is_ok)
+                        .map(Result::unwrap_or_default)
+                        .enumerate()
+                        .map(|(id, time)| TestResult { time, id })
+                        .collect();
+                }
+            }
+        }
+    }
+    vec![]
+}
+
 #[component]
 pub fn HomePage(cx: Scope) -> impl IntoView {
     let (time, set_time) = create_signal(cx, 0i64);
     let (running, set_running) = create_signal(cx, false);
     let (start_time, set_start_time) = create_signal(cx, get_now());
-    let (results, set_results) = create_signal(cx, Vec::<TestResult>::new());
+    let (results, set_results) = create_signal(cx, get_stored_results());
     let (early_click, set_early_click) = create_signal(cx, false);
     let (next_start_time, set_next_start_time) = create_signal(cx, get_next_start_time());
+
+    let storage = leptos::window().local_storage();
+
+    create_effect(cx, move |_| {
+        if let Ok(storage) = &storage {
+            if let Some(storage) = storage {
+                let results_str: String = results()
+                    .iter()
+                    .map(|result| result.time.to_string() + "\n")
+                    .collect();
+                _ = storage.set("results", &results_str);
+            }
+        }
+    });
 
     _ = set_interval(
         move || {
@@ -42,14 +79,6 @@ pub fn HomePage(cx: Scope) -> impl IntoView {
         },
         std::time::Duration::from_millis(10),
     );
-
-    // set_timeout(
-    //     move || {
-    //         set_running(true);
-    //         set_start_time(chrono::Utc::now().timestamp_millis());
-    //     },
-    //     std::time::Duration::from_millis(rand::thread_rng().gen_range(1500, 5000)),
-    // );
 
     view! { cx,
         <div class="app">
@@ -84,10 +113,6 @@ pub fn HomePage(cx: Scope) -> impl IntoView {
                         set_running(false);
                         set_start_time(now);
                         set_next_start_time(get_next_start_time());
-                        // set_timeout(move || {
-                        //     set_running(true);
-                        //     set_start_time(chrono::Utc::now().timestamp_millis());
-                        // }, std::time::Duration::from_millis(rand::thread_rng().gen_range(1500, 5000)));
                     } else {
                         if !early_click() {
                             set_results.update(|results| results.push(TestResult { id: results.len(), time: -1 }));
